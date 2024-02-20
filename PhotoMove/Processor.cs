3d5f94bf25d70683bd129a78be9346f8c70b1687
Jeremy Benscoter <jbenscoter@pourovergames.com>
2024-02-19 22:00:30 -0500

    initial commit

---
+++ PhotoMove/Processor.cs
@@ -0,0 +1,120 @@
+﻿using MetadataExtractor.Formats.Exif;
+using MetadataExtractor.Formats.QuickTime;
+
+namespace PhotoMove
+{
+
+    internal abstract class Processor
+    {
+        private static int[] DATE_TAG_IDS = new int[] {
+            QuickTimeMovieHeaderDirectory.TagCreated, ExifDirectoryBase.TagDateTimeOriginal, ExifDirectoryBase.TagDateTime,
+            QuickTimeMetadataHeaderDirectory.TagCreationDate
+        };
+
+        private static int[] DEVICE_TAG_IDS = new int[]
+        {
+            ExifDirectoryBase.TagCameraOwnerName, ExifDirectoryBase.TagMake, ExifDirectoryBase.TagModel,
+            QuickTimeMetadataHeaderDirectory.TagArtist, QuickTimeMetadataHeaderDirectory.TagMake,
+            QuickTimeMetadataHeaderDirectory.TagModel,
+
+        };
+
+        public Options Options { get; set; }
+
+        public Processor(Options options)
+        {
+            Options = options;
+        }
+
+        public bool Move(DateTime date, string rootPath, string filePath, string name)
+        {
+
+            string year = date.Year.ToString();
+            string month = date.Month.ToString("00");
+            string day = date.Day.ToString("00");
+
+            string newPath = Path.Combine(rootPath, year);
+            if (!Directory.Exists(newPath))
+            {
+                Directory.CreateDirectory(newPath);
+            }
+            newPath = Path.Combine(rootPath, year, month);
+            if (!Directory.Exists(newPath))
+            {
+                Directory.CreateDirectory(newPath);
+            }
+            newPath = Path.Combine(rootPath, year, month, day);
+            if (!Directory.Exists(newPath))
+            {
+                Directory.CreateDirectory(newPath);
+            }
+            newPath = Path.Combine(rootPath, year, month, day, name);
+
+            try
+            {
+                File.Move(filePath, newPath, false);
+                Console.WriteLine($"File moved from {filePath} to {newPath}.");
+                return true;
+            }
+            catch (IOException iox)
+            {
+                return false;
+            }
+        }
+
+        public static string TryGetDeviceInfo(IReadOnlyList<MetadataExtractor.Directory> directories)
+        {
+            string? make = null;
+            string? model = null;
+
+            foreach (var d in directories)
+            {
+                try
+                {
+                    make = make ?? d.GetDescription(ExifDirectoryBase.TagMake);
+                    model = model ?? d.GetDescription(ExifDirectoryBase.TagModel);
+
+                    make = make ?? d.GetDescription(QuickTimeMetadataHeaderDirectory.TagMake);
+                    model = model ?? d.GetDescription(QuickTimeMetadataHeaderDirectory.TagModel);
+
+                    if (make != null && model != null)
+                    {
+                        break;
+                    }
+                }
+                catch (Exception ex) { }
+
+            }
+
+
+            make = make ?? "unknown";
+            model = model ?? "unknown";
+
+            string deviceDetails = $"{make}-{model}";
+            deviceDetails = deviceDetails.Replace(" ", ".").ToLower();
+
+            return deviceDetails;
+        }
+
+        public static DateTime TryGetDateTime(IReadOnlyList<MetadataExtractor.Directory> directories)
+        {
+            foreach (var d in directories)
+            {
+                foreach (var t in DATE_TAG_IDS)
+                {
+                    try
+                    {
+                        if (MetadataExtractor.DirectoryExtensions.TryGetDateTime(d, t, out DateTime date))
+                        {
+                            return date;
+                        }
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+
+            return DateTime.MinValue;
+        }
+
+    }
+}
---
+++ PhotoMove/Worker.cs
@@ -0,0 +1,60 @@
+﻿namespace PhotoMove
+{
+    internal class Worker
+    {
+        public Options Options { get; private set; }
+        public int TotalFileCount { get; private set; }
+
+        public Worker(Options options)
+        {
+            Options = options;
+        }
+
+        public int DoWork(string path, string dest)
+        {
+            int count = 0;
+
+            var imageProcessor = new ImageProcessor(Options);
+            var quicktimeProcessor = new QuicktimeProcessor(Options);
+            var directories = Directory.GetDirectories(path);
+
+            foreach (var filePath in Directory.GetFiles(path))
+            {
+                var file = new FileInfo(filePath);
+                IReadOnlyList<MetadataExtractor.Directory> dirs;
+                string dateTimeText = "";
+
+                if (file.Extension.Equals(".yml", StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+                if (file.Extension.Equals(".db", StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+                else if (imageProcessor.TryProcess(dest, filePath))
+                {
+                    count++;
+                }
+                else if (quicktimeProcessor.TryProcess(dest, filePath))
+                {
+                    count++;
+                }
+                TotalFileCount++;
+            }
+
+            if (Options.Recursive)
+            {
+                foreach (var directory in directories)
+                {
+                    if (directory != dest)
+                    {
+                        count += DoWork(directory, dest);
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
